ca9a4ed57547d0ec5095ae6954c8234bd4fbb379
ahmed-838 <ashams3719262@gmail.com>
2025-01-25 18:58:50 +0200

    Save changes before prebuild

--- android/app/src/main/java/com/ahmed_shams2/gencare/MainActivity.kt
+++ android/app/src/main/java/com/ahmed_shams2/gencare/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.anonymous.gencare
+package com.ahmed_shams2.gencare
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/com/ahmed_shams2/gencare/MainApplication.kt
+++ android/app/src/main/java/com/ahmed_shams2/gencare/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.anonymous.gencare
+package com.ahmed_shams2.gencare
 
 import android.app.Application
 import android.content.res.Configuration
