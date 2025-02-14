ec9faf77c4ecdda8309eb5ff41aa618b7d2f9b3f
ahmed-838 <ashams3719262@gmail.com>
2025-02-14 22:25:33 +0200

    updated weeks && disease saving && babynames saving , just still notification and accounts verifications

--- app.config.js
+++ app.config.js
@@ -1,12 +1,45 @@
-module.exports = {
+export default {
   expo: {
     name: 'GenCare',
     slug: 'gencare',
     version: '1.0.0',
     scheme: 'gencare',
+    icon: "./assets/Logo/Mob-Logo-removebg-preview.png",
     extra: {
       apiUrl: 'http://192.168.122.1:5000',
+      eas: {
+        projectId: "492afcaf-713b-4b0c-b955-f70442f2acdb"
+      }
     },
-    newArchEnabled: true
+    newArchEnabled: true,
+    android: {
+      package: "com.ahmed_shams2.gencare",
+      compileSdk: 34,
+      targetSdk: 34,
+      buildToolsVersion: "34.0.0",
+      adaptiveIcon: {
+        foregroundImage: "./assets/Logo/Mob-Logo-removebg-preview.png",
+        backgroundColor: "#ffffff"
+      },
+      ndkVersion: "25.1.8937393",
+      minSdkVersion: 21,
+      buildConfigExtra: {
+        repositories: [
+          "maven { url 'https://www.jitpack.io' }"
+        ]
+      }
+    },
+    ios: {
+      bundleIdentifier: "com.ahmed-shams2.gencare",
+      supportsTablet: true
+    },
+    plugins: [
+      [
+        'expo-blur',
+        {
+          enabled: false
+        }
+      ]
+    ]
   },
 }; 
